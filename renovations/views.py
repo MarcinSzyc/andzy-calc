1cf81434211b2a13cea7d929de1eaa1d0e8b0bd2
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-24 17:57:05 +0200

    added generic product list view

--- renovations/views.py
+++ renovations/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from renovations.models import Product
+
 
 # Create your views here.
+class AllProducts(ListView):
+    model = Product
