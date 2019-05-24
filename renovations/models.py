e4e0fb7354a5995831cfcc64f2012c17388043f5
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-24 16:30:36 +0200

    product model added

--- renovations/models.py
+++ renovations/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+TYPE = (
+    (1, "Armature"),
+    (2, "Tiles"),
+    (3, "Paint"),
+    (4, "Other"),
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE)
