d4f1be221ed871cecc7ceec3b3770e90f0b588f7
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-27 22:21:20 +0200

    context processor added to settings

--- andzy_calc/settings.py
+++ andzy_calc/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "renovations.form_context_processor.product_form_context"
             ],
         },
     },
