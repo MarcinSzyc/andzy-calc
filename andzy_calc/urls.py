9606cf89267a6ae3750f7d1ca10cb15d6047d275
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-27 14:51:35 +0200

    added default view to urls

--- andzy_calc/urls.py
+++ andzy_calc/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from renovations.views import AllProducts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', AllProducts.as_view(), name='home'),
+    path('renovations/', include('renovations.urls')),
 ]
