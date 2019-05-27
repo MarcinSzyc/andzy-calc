b752f9494ef0e707c9eb0281db4137fc42acf0ad
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-27 14:47:25 +0200

    added or modified delete, udpate, new product urls

--- renovations/urls.py
+++ renovations/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from renovations.views import AllProducts
+from renovations.views import AllProducts, ProductNew, ProductUpdate, ProductDelete
 
 app_name = "renovations"
 
 urlpatterns = [
     path('products/', AllProducts.as_view(), name='all-products'),
+    path('products/new/', ProductNew.as_view(), name='new-product'),
+    path('products/update/<int:pk>/', ProductUpdate.as_view(), name='update-product'),
+    path('products/delete/<int:pk>/', ProductDelete.as_view(), name='delete-product'),
 ]
