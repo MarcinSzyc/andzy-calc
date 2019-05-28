55899fa0241a5b6808a7b609aa8a97255a4110e7
MarcinSzyc <szyc.marcin@gmail.com>
2019-05-28 13:04:25 +0200

    changed description filed size

--- renovations/forms.py
+++ renovations/forms.py
@@ -1,8 +1,12 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 from renovations.models import Product
 
 
 class NewProductForm(ModelForm):
     class Meta:
         model = Product
+        widgets = {
+            "description": Textarea(attrs={'rows': 5, 'cols': 20})
+        }
         exclude = []
+
