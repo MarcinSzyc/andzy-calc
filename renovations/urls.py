943e2d75e31da0a018aacecd8cac606e3b2b9518
MarcinSzyc <szyc.marcin@gmail.com>
2019-06-03 21:30:48 +0200

    product handling added to views and urls

--- renovations/urls.py
+++ renovations/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from renovations.views import AllProducts, ProductNew, ProductUpdate, ProductDelete
+from renovations.views import (
+    AllProducts, ProductNew, ProductUpdate, ProductDelete, AllRooms,
+    RoomNew, RoomUpdate, RoomDelete, EditRoom)
 
 app_name = "renovations"
 
@@ -8,4 +10,8 @@ urlpatterns = [
     path('products/new/', ProductNew.as_view(), name='new-product'),
     path('products/update/<int:pk>/', ProductUpdate.as_view(), name='update-product'),
     path('products/delete/<int:pk>/', ProductDelete.as_view(), name='delete-product'),
+    path('rooms/', AllRooms.as_view(), name='all-rooms'),
+    path('rooms/new/', RoomNew.as_view(), name='new-room'),
+    path('rooms/update/<int:pk>/', RoomUpdate.as_view(), name='update-room'),
+    path('rooms/delete/<int:pk>/', RoomDelete.as_view(), name='delete-room'),
 ]
--- renovations/views.py
+++ renovations/views.py
@@ -1,11 +1,11 @@
-from django.views.generic import ListView, View
-from renovations.models import Product, PRODUCT_TYPE
-from django.views.generic.edit import CreateView, UpdateView, BaseDeleteView
+from django.views.generic import ListView, View, DetailView
+from renovations.models import Product, Room, Renovation, PRODUCT_TYPE, ROOM_TYPE
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import redirect
 from django.contrib import messages
-from renovations.forms import NewProductForm
+from renovations.forms import NewRoomForm
 
 
 class AllProducts(ListView):
@@ -23,7 +23,7 @@ class AllProducts(ListView):
 
 class ProductNew(SuccessMessageMixin, CreateView):
     model = Product
-    fields = ['name', 'description', 'type']
+    fields = '__all__'
     template_name = 'renovations/products_view.html'
     success_url = reverse_lazy('renovations:all-products')
     success_message = 'BOOM! Product created successfully!!'
@@ -31,7 +31,7 @@ class ProductNew(SuccessMessageMixin, CreateView):
 
 class ProductUpdate(SuccessMessageMixin, UpdateView):
     model = Product
-    fields = ['name', 'description', 'type']
+    fields = '__all__'
     success_url = reverse_lazy('renovations:all-products')
     success_message = 'BOOM! Product updated successfully!!'
 
@@ -43,3 +43,53 @@ class ProductDelete(View):
         product_instance.delete()
         messages.error(request, "BOOM! Product deleted successfully")
         return redirect('renovations:all-products')
+
+
+class AllRooms(ListView):
+    model = Room
+    template_name = 'renovations/room_list.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        # context['new_product_form'] = NewProductForm
+        context['ROOM_TYPE'] = ROOM_TYPE
+        return context
+
+
+class EditRoom(UpdateView):
+    form_class = NewRoomForm
+    template_name = 'renovations/edit_room.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['ROOM_TYPE'] = ROOM_TYPE
+        return context
+
+
+
+class RoomNew(SuccessMessageMixin, CreateView):
+    model = Room
+    fields = '__all__'
+    template_name = 'renovations/products_view.html'
+    success_url = reverse_lazy('renovations:all-rooms')
+    success_message = 'BOOM! Room created successfully!!'
+
+
+class RoomUpdate(SuccessMessageMixin, UpdateView):
+    model = Room
+    form_class = NewRoomForm
+    template_name = 'renovations/edit_room.html'
+    success_url = reverse_lazy('renovations:all-rooms')
+    success_message = 'BOOM! Room updated successfully!!'
+
+
+class RoomDelete(View):
+
+    def get(self, request, pk):
+        room_instance = Room.objects.get(pk=pk)
+        room_instance.delete()
+        messages.error(request, "BOOM! Room deleted successfully")
+        return redirect('renovations:all-rooms')
