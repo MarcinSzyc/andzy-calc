d922222115023082a3a02811f68fc4cbd778201d
MarcinSzyc <szyc.marcin@gmail.com>
2019-06-13 10:09:43 +0200

    cost form added to context processor

--- renovations/form_context_processor.py
+++ renovations/form_context_processor.py
@@ -1,10 +1,11 @@
-from renovations.forms import NewProductForm, NewRoomForm, NewRenovationForm
+from renovations.forms import NewProductForm, NewRoomForm, NewRenovationForm, NewCostForm
 
 
 def renovations_form_context(request):
     result = {
         'new_product_form': NewProductForm,
         'new_room_form': NewRoomForm,
-        'new_renovation_form': NewRenovationForm
+        'new_renovation_form': NewRenovationForm,
+        'new_cost_form': NewCostForm
     }
     return result
